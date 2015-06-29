b1234817fb601efe87437c67454da9850c273027
Faerbit <faerbit@gmail.com>
2015-06-30 00:46:36 +0200

    Fixing typos in 32bit-gdt.asm

--- 32bit-gdt.asm
+++ 32bit-gdt.asm
@@ -9,7 +9,7 @@ gdt_start:
 gdt_code:
     dw 0xffff       ; segment length, bits 0-15
     dw 0x0          ; segment base bits 0-15
-    dw 0x0          ; segment base bits 16-23
+    db 0x0          ; segment base bits 16-23
     db 10011010b    ; flags (8 bits)
     db 11001111b    ; flags (4 bits) + segment length, bits 16-19
     db 0x0          ; segment base, bits 24-31
@@ -20,7 +20,7 @@ gdt_code:
 gdt_data:
     dw 0xffff
     dw 0x0
-    dw 0x0
+    db 0x0
     db 10010010b
     db 11001111b
     db 0x0
@@ -33,4 +33,4 @@ gdt_descriptor:
 
 ; define some constants for later use
 CODE_SEG equ gdt_code - gdt_start
-DATA SEG equ gdt_data - gdt_start
+DATA_SEG equ gdt_data - gdt_start
